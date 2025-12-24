cfb3e12b7ec36d64baaed4596e550b73d8ae7e39
solosboris <solosboris@gmail.com>
2025-12-24 15:27:46 +0100

    users: developed app building CI action

--- src/e2e/users.spec.ts
+++ src/e2e/users.spec.ts
@@ -1,6 +1,24 @@
 import { test, expect } from '@playwright/test'
 
 test('users page loads', async ({ page }) => {
-    await page.goto('http://localhost:5173')
-    await expect(page.getByText('Users')).toBeVisible()
+  await page.route('**/ahead/users', route =>
+    route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      body: JSON.stringify([
+        {
+          id: 1,
+          firstName: 'John',
+          lastName: 'Doe',
+          email: 'john@doe.com'
+        }
+      ])
+    })
+  )
+
+  await page.goto('http://localhost:5173')
+
+  await expect(
+    page.getByRole('heading', { name: 'Users' })
+  ).toBeVisible()
 })
